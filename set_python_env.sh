67398e86017401a06754e1f481eacecda5ceaf96
Brian Cristante <brcrista@microsoft.com>
2018-01-26 17:51:54 -0500

    move pylintrc

--- set_python_env.sh
+++ set_python_env.sh
@@ -1,4 +1,4 @@
 export PROJECT_ROOT=/Users/brian/Code/ProjectEuler/src
 export PYTHONPATH=$PROJECT_ROOT:$PROJECT_ROOT/euler
 export MYPYPATH=$PYTHONPATH
-export PYLINTRC=$PROJECT_ROOT/config/pylintrc
+export PYLINTRC=$PROJECT_ROOT/pylintrc
