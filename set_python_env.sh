2a4db992956696c514c1bce9601b144ebb37b4e8
Brian Cristante <brcrista@microsoft.com>
2018-01-26 15:52:57 -0500

    Updating PYTHONPATH

--- set_python_env.sh
+++ set_python_env.sh
@@ -1,4 +1,4 @@
 export PROJECT_ROOT=/Users/brian/Code/ProjectEuler/src
-export PYTHONPATH=$PROJECT_ROOT/core
+export PYTHONPATH=$PROJECT_ROOT
 export MYPYPATH=$PYTHONPATH
 export PYLINTRC=$PROJECT_ROOT/config/pylintrc
