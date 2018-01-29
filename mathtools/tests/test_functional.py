bbce4885e47306f8b5579b3e494126f0a5a8f102
Brian Cristante <brcrista@microsoft.com>
2018-01-29 11:32:07 -0500

    Add argmin tests. Remove doctests from unit tests

--- mathtools/tests/test_functional.py
+++ mathtools/tests/test_functional.py
@@ -4,10 +4,12 @@ from mathtools.functional import *
 
 class Test(unittest.TestCase):
     def test_argmax(self):
-        self.assertEqual(argmax(lambda x: x, [0, 1, 5, 3]), 5)
-        self.assertEqual(argmax(lambda x, y: x + y, [(0, 1), (1, 5), (3, 2)]), (1, 5))
-        self.assertEqual(argmax(lambda x: x, ascii_lowercase), 'z')
+        self.assertEqual(argmax(identity, ascii_lowercase), 'z')
         self.assertEqual(argmax(lambda x: x * x, range(-5, 6)), -5)
+
+    def test_argmin(self):
+        self.assertEqual(argmin(identity, ascii_lowercase), 'a')
+        self.assertEqual(argmin(lambda x: x * x, range(-5, 6)), 0)
 
 if __name__ == '__main__':
     unittest.main()
