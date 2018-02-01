c8d4e03629245febf921de29f346218978dd9b75
Brian Cristante <brcrista@microsoft.com>
2018-02-01 15:53:33 -0500

    Removing set environment scripts, which should no longer be useful

--- Set-PythonEnv.ps1
+++
@@ -1,3 +0,0 @@
-$env:PROJECT_ROOT="E:\Python\Project Euler\src"
-$env:MYPYPATH="$env:PYTHONPATH"
-$env:PYLINTRC="$env:PROJECT_ROOT\pylintrc"
--- set_python_env.sh
+++
@@ -1,3 +0,0 @@
-export PROJECT_ROOT=/Users/brian/Code/ProjectEuler/src
-export MYPYPATH=$PYTHONPATH
-export PYLINTRC=$PROJECT_ROOT/pylintrc
