3632866dc44ea231ddeebc9ad0bc55e0d9ab03bc
Brian Cristante <brian.cristante@gmail.com>
2018-05-18 07:15:49 -0400

    Revert "Bump version"
    This reverts commit fc91d4cf2d58b471bf4ca04c041e3aa9e33bc2b9.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mathtools',
-    version='0.4.1',
+    version='0.4.0',
     description='A library for doing discrete math in Python',
     packages=['mathtools'],
     python_requires='~=3.6',
