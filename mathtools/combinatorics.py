c281b8c2b6a94d20c5eb7cc493ee610e6317ee89
Brian Cristante <brian.cristante@gmail.com>
2018-05-17 22:55:44 -0400

    Rework recurrence to work with an arbitrary number of previous elements in the sequence

--- mathtools/combinatorics.py
+++ mathtools/combinatorics.py
@@ -1,31 +1,39 @@
 from math import factorial
-from typing import Callable, Iterator, Optional, TypeVar
+from typing import Callable, Iterator, List, Optional, TypeVar
 from mathtools import product
 
 _T = TypeVar('_T')
 
-def recurrence(start: _T, func: Callable[[_T], Optional[_T]]) -> Iterator[_T]:
+def recurrence(start: List[_T], func: Callable[..., Optional[_T]]) -> Iterator[_T]:
     """
     Return an `Iterator` beginning with `start` where each element is created
-    by applying `func` to the previous element.
+    by applying `func` to the previous `arity` elements.
 
+    `func()` should take `arity` parameters of type `_T`.
     Iteration stops when a `None` value is reached.
 
-    >>> list(recurrence(None, lambda: 100))
-    []
+    >>> import itertools
+    >>> it = recurrence([], lambda: 100)
+    >>> list(itertools.islice(it, 3))
+    [100, 100, 100]
 
-    >>> list(recurrence(1, lambda x: x + 1 if x < 5 else None))
+    >>> list(recurrence([1], lambda x: x + 1 if x < 5 else None))
     [1, 2, 3, 4, 5]
 
     >>> import itertools
-    >>> it = recurrence(1, lambda x: x * 2)
+    >>> it = recurrence([1], lambda x: x * 2)
     >>> list(itertools.islice(it, 5))
     [1, 2, 4, 8, 16]
     """
-    current: Optional[_T] = start
-    while current is not None:
-        yield current
-        current = func(current)
+    window = start
+    yield from iter(start)
+
+    value = func(*window)
+    while value is not None:
+        yield value
+        window += [value]
+        window = window[1:]
+        value = func(*window)
 
 def fibonacci_numbers() -> Iterator[int]:
     """
