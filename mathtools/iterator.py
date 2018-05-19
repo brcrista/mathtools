b372f4a1c5c31a6909fc7306ec869bfd6eeca1ff
Brian Cristante <brian.cristante@gmail.com>
2018-05-19 11:18:49 -0400

    Add `unzip` function and `test_iterable`

--- mathtools/iterator.py
+++ mathtools/iterator.py
@@ -1,8 +1,12 @@
 import itertools
-from typing import Iterable, List, TypeVar
+from typing import Iterable, Iterator, List, Tuple, TypeVar
 
 _T = TypeVar('_T')
 
 def take(n: int, iterable: Iterable[_T]) -> List[_T]:
     "Return the first `n` items of the iterable as a list"
     return list(itertools.islice(iterable, n))
+
+def unzip(tuples: Iterator[Tuple]) -> List[Iterable]:
+    """The inverse of the `zip` built-in function."""
+    return list(zip(*tuples))
---
+++ tests/test_iterable.py
@@ -0,0 +1,13 @@
+from mathtools.iterator import take, unzip
+
+def test_take():
+    assert take(0, range(0, 10)) == []
+    assert take(5, [1, 2, 3]) == [1, 2, 3]
+    assert take(5, (2 * i for i in range(0, 100))) == [0, 2, 4, 6, 8]
+
+def test_unzip():
+    xs = [1, 2, 3]
+    ys = [4, 5, 6]
+    _xs, _ys = unzip(zip(xs, ys))
+    assert list(_xs) == xs
+    assert list(_ys) == ys
